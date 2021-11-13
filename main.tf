4f4881fd22c0ffc2e0c3dfd87fecd21a59dce6f9
stepapp161 <88939739+stepapp161@users.noreply.github.com>
2021-11-13 13:21:10 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_lb_target_group" "target_group" {
   vpc_id      = "${aws_default_vpc.default_vpc.id}" # Reference the default VPC
   health_check {
     matcher = "200,301"
-    path = "/"
+    path = "/sample/"
   }
 }
 
