4f257d804210d6bb3f07b175d527ee936598ca84
stepapp161 <88939739+stepapp161@users.noreply.github.com>
2021-11-15 10:59:29 -0600

    Update service_security_group.tf

--- service_security_group.tf
+++ service_security_group.tf
@@ -1,16 +1,7 @@
 resource "aws_security_group" "service_security_group" {
-  ingress {
     security_groups = "${aws_security_group.load_balancer_security_group.id}"
-  }
-
-egress {
-  }
 }
 
 resource "aws_security_group" "load_balancer_security_group" {
-  ingress {
-  }
 
-  egress {
-  }
 }
