b080fcce62ff5265dffdf8ec8a0cf059904c7837
stepapp161 <88939739+stepapp161@users.noreply.github.com>
2021-11-18 08:41:08 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,15 +34,15 @@ resource "aws_ecs_task_definition" "my_first_task" {
           "hostPort": 3000
         }
       ],
-      "memory": 1,
-      "cpu": 512
+      "memory": 512,
+      "cpu": 256
     }
   ]
 DEFINITION
   requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is a  Fargate requirement
-  memory                   = 1        # Specifying the memory our container requires
-  cpu                      = 512         # Specifying the CPU our container requires
+  memory                   = 512      # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
